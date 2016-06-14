a59e37021677fe215c042bc5ba7092b046ef345f
KacperSiuda <kacper.siuda@hotmail.com>
2016-06-15 00:05:30 +0200

    initialize CMS project

---
+++ CMS/CMS/CMS/views.py
@@ -0,0 +1,11 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from CMS import app
+
+@app.route('/')
+def home():
+  return "Hello world!"
